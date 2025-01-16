f3df5d4d7731e6546d8978183d2fe170adb9594c
Juan Sensio <sensio.juan@gmail.com>
2025-01-16 22:31:24 +0100

    update

--- src/GameEngine.cpp
+++ src/GameEngine.cpp
@@ -13,7 +13,7 @@ void GameEngine::run() {
 	float screenWidth = GetScreenWidth();
 	float screenHeight = GetScreenHeight();
 	float x = 20;
-	float v = 200; // px/s
+	float v = 300; // px/s
 	int frame = 0;
 	float lastTime = GetTime();
     while (!WindowShouldClose())   
@@ -26,7 +26,14 @@ void GameEngine::run() {
 		x += v * deltaTime;
 		if (x > screenWidth - 30) v = 0;
 		frame += 1;
-        DrawCircle(x, screenHeight/2, 10, WHITE);
+		// Colisión
+		// if (x >= screenWidth/2 - 50 && x <= screenWidth/2 - 40 && 
+		// 	screenHeight/2 - 10 <= screenHeight/2 + 50 && screenHeight/2 + 10 >= screenHeight/2 - 50) {
+		// 	x = screenWidth/2 - 60;
+		// 	v = 0;
+		// }
+		DrawCircle(x, screenHeight/2, 10, WHITE);
+		// DrawRectangle(screenWidth/2 - 50, screenHeight/2 - 50, 10, 100, WHITE);
 		DrawText(TextFormat("Frame %d", frame), 10, 10, 20, WHITE);
         DrawFPS(10, 40);
         EndDrawing();
