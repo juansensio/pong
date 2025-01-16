d5c148908e865acec1d5665286c4795667b52b34
Juan Sensio <sensio.juan@gmail.com>
2025-01-16 22:26:38 +0100

    update

--- src/GameEngine.cpp
+++ src/GameEngine.cpp
@@ -5,7 +5,7 @@
 void GameEngine::init() {
 	SetConfigFlags(FLAG_MSAA_4X_HINT | FLAG_VSYNC_HINT | FLAG_WINDOW_RESIZABLE);
 	InitWindow(1270, 720, "pong");
-    SetTargetFPS(10);               
+    SetTargetFPS(3);               
 }
 
 void GameEngine::run() {
@@ -13,13 +13,17 @@ void GameEngine::run() {
 	float screenWidth = GetScreenWidth();
 	float screenHeight = GetScreenHeight();
 	float x = 20;
-	float v = 10;
+	float v = 200; // px/s
 	int frame = 0;
+	float lastTime = GetTime();
     while (!WindowShouldClose())   
     {
         BeginDrawing();
         ClearBackground(BLACK);
-		x += v;
+		float currentTime = GetTime();
+		float deltaTime = currentTime - lastTime;
+		lastTime = currentTime;
+		x += v * deltaTime;
 		if (x > screenWidth - 30) v = 0;
 		frame += 1;
         DrawCircle(x, screenHeight/2, 10, WHITE);
