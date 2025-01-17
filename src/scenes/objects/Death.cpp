ab8d3c5ff73d54fd4ef8c517a5cc52abb687d82b
Juan Sensio <sensio.juan@gmail.com>
2025-01-17 19:59:54 +0100

    update

--- src/scenes/objects/Death.cpp
+++ src/scenes/objects/Death.cpp
@@ -4,8 +4,8 @@ void Death::init()
 {
 	float screenHeight = GetScreenHeight();
 	float screenWidth = GetScreenWidth();
-	// _entity->add<CTransform>(Vector2{screenWidth - 5, screenHeight / 2}, Vector2{0, 0});
-	_entity->add<CTransform>(Vector2{screenWidth + 15, screenHeight / 2}, Vector2{0, 0});
+		// _entity->add<CTransform>(Vector2{5, screenHeight / 2}, Vector2{0, 0});
+	_entity->add<CTransform>(Vector2{-15, screenHeight / 2}, Vector2{0, 0});
 	_entity->add<CBoundingBox>(Vector2{10, screenHeight});
 }
 
